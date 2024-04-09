779ad2aae2fd8311093d97653893dc39e03e338e
Rafael Rodrigues <rafaelrodriguesmirandadiniz@gmail.com>
2024-04-09 15:33:58 -0300

    First Commit

---
+++ RPGConsoleGame/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RPGConsoleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose your CLASS: [K]night - [M]age - [A]rcher");
+            string playerClass = Console.ReadLine(); 
+        }
+    }
+}
